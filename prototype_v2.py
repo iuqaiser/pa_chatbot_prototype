981c8fa1fe72b7ff753ba4738626fe709db4372f
Iman Umair-Qaiser <imanqaiser03@gmail.com>
2023-09-14 02:57:32 -0400

    Create prototype_v2.py

---
+++ prototype_v2.py
@@ -0,0 +1,2 @@
+#based on https://www.techwithtim.net/tutorials/ai-chatbot
+#goal: create program, build with streamlit, deploy with either streamlit community cloud or other web app deployment service
